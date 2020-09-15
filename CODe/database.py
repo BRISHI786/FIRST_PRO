57c3422711cb9fbdc96fac952072c50623968199
RISHI <rishi.kumar.3098@gmail.com>
2020-09-15 14:07:54 +0530

    database added

--- CODe/database.py
+++ CODe/database.py
@@ -1,1 +1,33 @@
-print("hello world")
+from sqlalchemy.orm import sessionmaker
+from projectorm import UserInput, Prediction
+from sqlalchemy import create_engine
+import streamlit as st# Streamliy
+
+engine = create_engine('sqlite:///utkarsh.sqlite3')
+Session = sessionmaker(bind = engine)
+sess = Session()
+
+st.title("Database with SQLalchemy")
+
+area = st.number_input('Area', value = 400)
+rooms = st.number_input('Number of rooms',max_value = 50,min_value = 1, value = 3)
+age =  st.number_input("Age of house")
+submit = st.button("Submit")
+
+if submit :
+    try :
+        entry = UserInput(house_area = area, no_of_rooms = rooms, age = age, location = "LUCKNOW")
+        sess.add(entry)#sess => database
+        sess.commit()
+        st.success("added to database")
+    except Exception as e:
+        st.error("hello Error")
+        st.error(e)
+
+if st.checkbox('view data') :
+    result = sess.query(UserInput).all()
+    for item in result :
+        st.subheader(item.location)
+        st.text(item.house_area)
+        st.text(item.no_of_rooms)
+        st.text(item.age)
---
+++ CODe/projectorm.py
@@ -0,0 +1,26 @@
+import sqlalchemy
+from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class UserInput(Base) :
+    '''HELLO WORLD ''' 
+    __tablename__ = 'userinputs'
+
+    id = Column(Integer, primary_key = True)
+    house_area = Column(Integer)
+    no_of_rooms = Column(Integer)
+    age = Column(Integer)
+    location = Column(String)
+    
+class Prediction(Base) :
+    __tablename__ = 'prediction'
+    
+    id = Column(Integer, primary_key = True)
+    result = Column(Integer)
+    input_id = Column(Integer, ForeignKey('userinputs.id'))
+    
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///utkarsh.sqlite3') # create Empty database name utkarsh.sqlite3
+    Base.metadata.create_all(engine) # create the above table
